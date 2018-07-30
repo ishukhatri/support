99142a331b7ed014e1b14803a2bedcd08b6c2893
Ishu Khatri <ishukhatri@gmail.com>
2018-07-30 14:35:50 +0530

    Greeting

---
+++ src/main/java/org/khatri/support/GreetingController.java
@@ -0,0 +1,15 @@
+package org.khatri.support;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetingController {
+
+    @GetMapping("/hi")
+    public String hello(){
+        return "Hi";
+    }
+}
